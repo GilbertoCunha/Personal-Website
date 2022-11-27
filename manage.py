f74559926ec8f03624e4062b194586ddae23b8a4
Gilberto Cunha <grncacad@gmail.com>
2022-11-27 21:22:22 +0000

    initial commit

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/generic", methods=["GET"])
+def generic():
+    return render_template("generic.html", title="Generic")
+
+@app.route("/elements", methods=["GET"])
+def elements():
+    return render_template("elements.html", title="Elements")
+
+if __name__ == "__main__":
+    app.run(debug=True)
